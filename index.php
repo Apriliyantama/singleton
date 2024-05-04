81e80103d8b99e4da56fe3ac929d8cd6077b9b9b
Apriliyantama <kokoapriliyantama@gmail.com>
2024-05-04 20:33:20 +0700

    changes

--- index.php
+++ index.php
@@ -10,21 +10,22 @@ $data = $read->readSepatu();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Shoe Database</title>
+    <title>Shoes List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/adidas.css' rel='stylesheet'>
 </head>
 <body>
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1 class="mt-5">Adidas Shoe</h1>
+            <h3 class="mt-5">Shoes Product</h1>
             <table class="table table-bordered mt-3">
                 <thead>
                 <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Merk</th>
+                    <th scope="col"><i class="gg-adidas"></i></th>
+                    <th scope="col">Brand</th>
                     <th scope="col">Model</th>
-                    <th scope="col">Warna</th>
+                    <th scope="col">Color</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -32,9 +33,9 @@ $data = $read->readSepatu();
                 <?php foreach ($data as $key => $value) : ?>
                     <tr>
                         <th scope="row"><?= $i++ ?></th>
-                        <td><?= $value['Merk'] ?></td>
+                        <td><?= $value['Brand'] ?></td>
                         <td><?= $value['Model'] ?></td>
-                        <td><?= $value['Warna'] ?></td>
+                        <td><?= $value['Color'] ?></td>
                     </tr>
                 <?php endforeach ?>
                 </tbody>
